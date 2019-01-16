54287f5048728cb15eff465ebbd5c25d65ea7d00
gaobin <gaobin@workway.com.cn>
2019-01-16 23:06:26 +0800

    rabbitmq 配置

---
+++ src/main/java/com/example/concurrency/rabbitmq/receiver/SecondConsumer.java
@@ -0,0 +1,20 @@
+package com.example.concurrency.rabbitmq.receiver;
+
+import com.example.concurrency.config.RabbitMqConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+* @Description: 消费者
+* @author gaobin
+* @createDate 2019/1/16 15:55
+*/
+@Component
+public class SecondConsumer {
+
+    @RabbitListener(queues = {"second-queue"})
+    public void handleMessage(String message) throws Exception {
+        // 处理消息
+        System.out.println("SecondConsumer {} handleMessage :"+message);
+    }
+}
