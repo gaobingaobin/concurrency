b6cd0a473388e86ba4abd54bb92c267be397261d
高彬 <gao.bin@angsivip.com>
2019-12-13 19:57:25 +0800

    测试

--- src/main/java/com/example/concurrency/controller/SendController.java
+++ src/main/java/com/example/concurrency/controller/SendController.java
@@ -1,6 +1,10 @@
 package com.example.concurrency.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.concurrency.rabbitmq.sender.FirstSender;
@@ -13,15 +17,19 @@ import java.util.UUID;
 * @createDate 2019/1/16 17:34
 */
 @RestController
+@Api(description = "消息发送")
 public class SendController {
     @Autowired
     private FirstSender firstSender;
 
-    @RequestMapping("/send")
+    @PostMapping("/send")
+    @ApiOperation(value = "发送消息")
     public String send(String message) {
         String uuid = UUID.randomUUID().toString();
         firstSender.send1(uuid, message);
         return uuid;
     }
+
+
 
 }
--- src/main/java/com/example/concurrency/controller/TestController.java
+++ src/main/java/com/example/concurrency/controller/TestController.java
@@ -5,14 +5,18 @@ import com.example.concurrency.redis.utils.RedisUtils;
 import com.example.concurrency.result.CodeMsg;
 import com.example.concurrency.result.Result;
 import com.example.concurrency.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 @Controller
+@Api(description = "测试")
 public class TestController {
 
     @Autowired
@@ -20,41 +24,53 @@ public class TestController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/get")
+    @GetMapping("/get")
     @ResponseBody
+    @ApiOperation(value = "redis get方法")
     public Object get(String k) {
         return redisUtils.get(k);
     }
 
-    @RequestMapping("/set")
+    @PostMapping("/set")
     @ResponseBody
+    @ApiOperation(value = "redis set方法")
     public boolean set(String k,String v) {
         return redisUtils.set(k,v);
     }
 
     @GetMapping("/success")
     @ResponseBody
+    @ApiOperation(value = "success")
     public Result<String> getSuccess(){
         return Result.success("hello");
     }
 
     @GetMapping("/error")
     @ResponseBody
+    @ApiOperation(value = "error")
     public Result<String> getError(){
         return Result.error(CodeMsg.SERVER_ERROR);
     }
 
     @GetMapping("/getUser")
     @ResponseBody
+    @ApiOperation(value = "getUser")
     public Result<User> getUserById(){
         User user = userService.getById(1);
         return Result.success(user);
     }
     @GetMapping("/setUser")
     @ResponseBody
+    @ApiOperation(value = "setUser")
     public Result<Boolean> setUser(){
         Boolean flag = userService.tx();
         return Result.success(flag);
+    }
+
+    @GetMapping("/forward")
+    @ApiOperation(value = "链接转发")
+    public String forward() {
+        return "redirect:https://www.baidu.com";
     }
 
 }
