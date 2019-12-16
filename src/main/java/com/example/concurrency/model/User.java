9b8a149e669f7be00d4cdbf3acd0938510c7f5e5
高彬 <gao.bin@angsivip.com>
2019-12-16 09:27:34 +0800

    加入热部署插件

--- src/main/java/com/example/concurrency/model/User.java
+++ src/main/java/com/example/concurrency/model/User.java
@@ -1,22 +1,12 @@
 package com.example.concurrency.model;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class User {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
