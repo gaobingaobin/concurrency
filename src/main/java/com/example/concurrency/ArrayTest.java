abad45951c9eb52d7959b50f093a425f968ca900
高彬 <gao.bin@angsivip.com>
2019-12-27 10:15:29 +0800

    算法题

--- src/main/java/com/example/concurrency/ArrayTest.java
+++ src/main/java/com/example/concurrency/ArrayTest.java
@@ -7,7 +7,9 @@ public class ArrayTest {
         Integer [] arr ={1,2,4,4,2,5};
         ArrayTest h = new ArrayTest();
         h.getTopSum(arr,2,4);
-        System.out.println("结果："+h.getTopSum(arr,2,3));
+        System.out.println("结果1："+h.getTopSum(arr,2,3));
+        int[] nums = {3,2,4};
+        System.out.println("结果2："+h.twoSum(nums,6).toString());
     }
 
 
@@ -47,5 +49,36 @@ public class ArrayTest {
             }
         }
         return kNumSum+mNumSum;
+    }
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     *示例:
+     *
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     *
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        int[] result = new int[2];
+        for(int i = 0;i<len;i++){
+            for(int j = 0;j<len-i-1;j++){
+                if(i==j){
+                    continue;
+                }
+                if(nums[i]+nums[j]==target){
+                 result = new int[]{i, j};
+                 return result;
+                }
+            }
+        }
+        return result;
     }
 }
